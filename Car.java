b078e503a4fdef5eed60b0475702c84d80433937
Sonja Dragicevic <dragicevic.sonja@hotmail.com>
2022-04-28 13:58:03 +0200

    Added calculation fuel conseptions according to distance

--- Car.java
+++ Car.java
@@ -1,21 +1,19 @@
 public class Car {
   private String model;
   private int buildYear;
-  private float comsuption;
-  private String color;
+  private float consuption;
   private double carSpeed;
-  private int CarSeat;
+  private boolean power;
+  private int fuelCapacity;
 
-  public Car() {}
 
-  public Car (String model, int buildYear, float comsuption, String color, double carSpeed, int CarSeat) {
+  public Car (boolean power, String model, int buildYear, float comsuption, double carSpeed, int fuelCapacity) {
+    this.setPower(power);
     this.setModel(model);
     this.setBuildYear(buildYear);
-    this.setComsuption(comsuption);
-    this.setColor(color);
+    this.setConsuption(comsuption);
     this.setCarSpeed(carSpeed);
-    this.setCarSeat(CarSeat);
-
+    this.setFuelCapacity(fuelCapacity);
   }
 
   public void setModel(String modelValue){
@@ -32,42 +30,90 @@ public class Car {
     return this.buildYear;
   }
 
-  public void setComsuption(Float fuel) {
-    this.comsuption = fuel;
+  public void setConsuption(Float fuel) {
+    this.consuption = fuel;
   }
   public Float getComsuption () {
-    return this.comsuption;
+    return this.consuption;
   }
-
-  public void setColor(String carColor) {
-    this.color = carColor;
-  }
-  public String getColor() {
-    return this.color;
-  }
+  
 
   public void setCarSpeed( Double speed) {
-    this.carSpeed = speed;
+    if (this.isOn()){
+      this.carSpeed = speed;
+    }
   }
   public Double getCarSpeed() {
     return this.carSpeed;
   }
 
-  public void setCarSeat( int seats){
-    this.CarSeat = seats;
-  }
-  public int getCarSeat() {
-    return this.CarSeat;
+  public void setPower(boolean power){
+    this.power = power;
   }
 
+  public boolean getPower(){
+    return this.power;
+  }
+
+  public boolean isOn() {
+    return this.power;
+  }
+
+  public Car model(String model) {
+    setModel(model);
+    return this;
+  }
+
+  public Car buildYear(int buildYear) {
+    setBuildYear(buildYear);
+    return this;
+  }
+
+  public Car comsuption(float consuption) {
+    setConsuption(consuption);
+    return this;
+  }
+
+
+  public Car carSpeed(double carSpeed) {
+    setCarSpeed(carSpeed);
+    return this;
+  }
+
+
+  public Car power(boolean power) {
+    setPower(power);
+    return this;
+  }
+
+  public void setFuelCapacity(int fuelCapacity) {
+    this.fuelCapacity = fuelCapacity;
+  }
+  public int getFuelCapacity(){
+    return this.fuelCapacity;
+  }
+
+  public void travelDistance(float distance){
+    System.out.println("Travel distance: " + distance );
+      final double carSpending = this.fuelCapacity/this.consuption*100;
+      final double chargingTimes = distance/carSpending;
+    System.out.println("For this distance you need how many tanks: " + chargingTimes);
+      double doubleNumber = chargingTimes;
+      int intPart = (int) doubleNumber;
+    System.out.println("Number of full tanks: " + intPart);
+    System.out.println("How many more l : " + (Math.ceil((doubleNumber - intPart)* this.fuelCapacity)) + "l");
+  }
 
   public void printInConsole() {
-    System.out.println("Model: " + this.getModel());
+    System.out.println("Model: " + this.model);
     System.out.println("Build year: " + this.buildYear);
-    System.out.println("Comsuption amount: " + this.comsuption + " per 100km");
-    System.out.println("Car color: " + this.color);
-    System.out.println("Car speed: " + this.carSpeed);
-    System.out.println("Car seats: "+ this.CarSeat);
+    System.out.println("Comsuption amount: " + this.consuption + " per 100km");
+    if(this.isOn()){
+      System.out.println("Car speed: " + this.carSpeed);
+    } else {
+      System.out.println("Car speed: off");
+    }
+    System.out.println("Fuel capacity: " + this.fuelCapacity);
     System.out.println();
   }
 }
--- Main.java
+++ Main.java
@@ -1,27 +1,33 @@
+import java.io.ObjectOutputStream.PutField;
+
 public class Main {
  public static void main(String[] args) {
      System.out.println("Here we start");
      System.out.println();
 
-     Car audi = new Car();
-     audi.setModel("Audi");
-     audi.setBuildYear(2021);
-     audi.setComsuption(6.5f);
-     audi.setColor("green");
-     audi.setCarSpeed(410.54);
-     audi.setCarSeat(5);
+     Car audi = new Car( true,"Audi", 2022, 8f, 350, 40);
      audi.printInConsole();
-
-     Car bmw = new Car();
-     bmw.setModel("BMW");
-     bmw.setBuildYear(2015);
-     bmw.setComsuption(7.5f);
-     bmw.setColor("red");
-     bmw.setCarSpeed(310.56);
-     bmw.setCarSeat(5);
-     bmw.printInConsole();
-
-     Car lada = new Car("Lada", 1998, 10.3f, "blue", 300.33, 4);
+     System.out.println("Calculation travel distance and fuel consumtions for audi");
+     audi.travelDistance(1650);
+     System.out.println();
+     
+     Car lada = new Car(false,"Lada", 1998, 10.5f, 400, 25);
      lada.printInConsole();
- }
+     System.out.println("Calculation travel distance and fuel consumtions for lada");
+     lada.travelDistance(2500);
+     System.out.println();
+     
+     Car bmw = new Car(true, "BMW", 2019, 9f, 300, 40);
+     bmw.printInConsole();
+     System.out.println("Calculation travel distance and fuel consumtions for bmw");
+     bmw.travelDistance(537);
+     System.out.println();
+     
+     Car peugeot = new Car(true, "Peugeot 308", 2018, 5f, 240, 30);
+     peugeot.printInConsole();
+     System.out.println("Calculation travel distance and fuel consumtions for peugeot");
+     peugeot.travelDistance(763);
+     System.out.println();
+
+    }
 }
